34f871e9ab66ced04394cbcf1c9b6b4ae183cf5f
marcushio <marcus.t.trujillo@gmail.com>
2020-10-30 18:55:42 -0600

    add some grammar

--- basicParser.hs
+++ basicParser.hs
@@ -1,19 +1,54 @@
 -- don't be basic, parse some Basic 
+import Text.ParserCombinators.Parsec 
+import Text.ParserCombinators.Parsec.Expr
+import Control.Monad 
 
 main = do 
     let test = "LET A = 2"
     print (test)
 
+--there's quite a few of these... but let's start with the pieces
+--we need to parse foo.bas and test.bas 
+--ultimately what are we parsing? In Scheme it was Sexpr. I think here it's Statements 
 
-data Value = Variable | Function | Constant -- or '(' <Expression> ')' but I'm not sure how to handle that 
+--Lance Provided Grammar 
+-- <Statement>   ::= DIM <Array List>
+--                 | END
+--                 | FOR ID '=' <Expression> TO <Expression>
+--                 | FOR ID '=' <Expression> TO <Expression> STEP <Expression>
+--                 | GOTO Integer
+--                 | GOSUB Integer
+--                 | IF <Expression> THEN Integer
+--                 | INPUT String ';' <ID List>
+--                 | LET <Variable> '=' <Expression>
+--                 | NEXT <ID List>
+--                 | ON <Expression> GOTO <Integer List>
+--                 | PRINT <Print list>
+--                 | PRINT TAB '(' <Expression> ')' <Print list>
+--                 | REM {Printable}*
+--                 | RETURN
+--                 | <Variable> '=' <Expression>
+data Statement = Let Variable Expression
 
 data Statements =  Statement ':' Statements | Statement
 
+-- <Expression>  ::= <And Exp> OR <Expression>
+--                 | <And Exp>
+data Expression = 
+
+data Expression = 
+
+data Value = Variable | Function | Constant -- | '(' <Expression> ')' but I'm not sure how to handle that 
+
+
 data Constant = Integer | String  
---there's quite a few of these... but let's start with the pieces
---we need to parse foo.bas and test.bas 
-data Statement = Let 
+
 
 -- do we have to make a lexer? 
+pStatement = do 
+    string "LET " 
+    var <- char
+    string " = " -- not a fan but this will eat what i want 
+    value <- 
 
 
